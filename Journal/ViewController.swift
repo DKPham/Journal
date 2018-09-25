2044071a7f9c0279446c840867ee3764f8bf9e77
Duy Pham <duypham@Duys-MacBook-Pro.local>
2018-09-25 13:14:23 -0400

    Initial Commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Duy Pham on 11/28/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
